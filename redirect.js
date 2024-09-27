c53953ed3cbf5fb1631aaa71c1c171464780ce71
bryku <drburnett@mail.com>
2024-09-26 21:03:02 -0700

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -2,8 +2,8 @@ function redirect(redirects){
     for(let redirect in redirects){
         if(location.pathname.endsWith(redirect)){
             let url = location.pathname.slice(0, -redirect.length + 1);
-						    url+= redirects[redirect] + location.search;
-						location.href = url;
-				}
+                url+= redirects[redirect] + location.search;
+                location.href = url;
+        }
     }
 }
